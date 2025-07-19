1bb1bfa61a2bf68e3e37834a399b7fcdbbc88f63
Shreya Singh <shreya.official.2.25@gmail.com>
2025-07-19 09:19:19 +0530

    new file:   003_Conditional_Statements_Loops/Qs_1_.cpp

---
+++ 003_Conditional_Statements_Loops/Qs_1_.cpp
@@ -0,0 +1,11 @@
+// Qs
+// Find character lowercase or uppercase
+
+// Qs
+// Sum of numbers from 1 to n.
+
+// Qs
+// Sum of all Odd numbers from 1 to N
+
+// Qs
+// Check if a number is prime or not.
