04c07e15b4168ce602e3ce798b5e2c387eb07591
Shreya Singh <shreya.official.2.25@gmail.com>
2025-07-30 11:09:36 +0530

    new file:   005_Functions/2_sum_mul.py

---
+++ 005_Functions/2_sum_mul.py
@@ -0,0 +1,20 @@
+
+
+def sum(a, b):
+    return a+b
+
+def mul(c, d):
+    return c*d
+
+
+o = int(input("Enter num 'o': "))
+p = int(input("Enter num 'p': "))
+print(sum(o, p))
+print(mul(o, p))
+
+
+if __name__ == "__main__":
+    m = int(input("Enter num: "))
+    n = int(input("Enter num: "))
+    print(sum(m, n))
+    print(mul(m, n))
