c414d463ac4ecf055ec7ee3ab4c27f53cc8c545f
Shreya Singh <shreya.official.2.25@gmail.com>
2025-07-30 11:09:03 +0530

    new file:   005_Functions/1_hello.py

---
+++ 005_Functions/1_hello.py
@@ -0,0 +1,17 @@
+
+
+def greet():
+    print("Hello Shreya!\n")
+greet()
+
+
+
+
+
+def greet():
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    print("Program Start")
+    greet()  # Function Call
+    print("Program End")
