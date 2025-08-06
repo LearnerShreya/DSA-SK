2f08282d9eea5a268e132b70a45ee64e6a6d0e08
Shreya Singh <shreya.official.2.25@gmail.com>
2025-08-06 07:54:20 +0530

    new file:   008_Array_Part_1/hw.py

---
+++ 008_Array_Part_1/hw.py
@@ -0,0 +1,6 @@
+# Homework
+# WAF to calculate sum & product of all numbers in an array.
+# WAF to swap the max & min number of an array.
+# WAF to print all the unique values in an array.
+# WAF to print intersection of 2 arrays.
+
