5d957a514767d6a62b492d4b3c2c91bb02457070
Shreya Singh <shreya.official.2.25@gmail.com>
2025-09-03 09:45:51 +0530

    new file:   016_Pointers_in_CPP/dereference.cpp

---
+++ 016_Pointers_in_CPP/dereference.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int a =10;
+    int* ptr = &a;
+    int** parPtr = &ptr;
+
+    cout<<&a<<endl;
+    cout<<ptr<<endl;
+    cout<<parPtr<<endl;
+
+    cout<<*(&a)<<endl;
+    cout<<*(ptr)<<endl;
+    cout<<*(parPtr)<<endl;
+    cout<<**(parPtr)<<endl;
+}
