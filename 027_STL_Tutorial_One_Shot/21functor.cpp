05d963d6553f9e75515d500fbace3a64e56fe1b2
Shreya Singh <shreya.official.2.25@gmail.com>
2025-11-02 10:38:47 +0530

    new file:   027_STL_Tutorial_One_Shot/21functor.cpp

---
+++ 027_STL_Tutorial_One_Shot/21functor.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    priority_queue<int, vector<int>, greater<int>> q;
+    q.push(5);
+    q.push(3);
+    q.push(10);
+    q.push(4);
+
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+    return 0;
+}
+
+
+
+// #include <iostream>
+// #include <queue>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     priority_queue<int> q;
+//     q.push(5);
+//     q.push(3);
+//     q.push(10);
+//     q.push(4);
+
+//     vector<int> v;
+//     while (!q.empty()) {
+//         v.push_back(q.top());
+//         q.pop();
+//     }
+
+//     for (int i = v.size() - 1; i >= 0; i--) {
+//         cout << v[i] << " ";
+//     }
+
+//     cout << endl;
+//     return 0;
+// }
