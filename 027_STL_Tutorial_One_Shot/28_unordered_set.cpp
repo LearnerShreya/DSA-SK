283c0e85b28162938cea458b56633f954d9717ba
Shreya Singh <shreya.official.2.25@gmail.com>
2025-11-05 20:47:16 +0530

    new file:   027_STL_Tutorial_One_Shot/28_unordered_set.cpp

---
+++ 027_STL_Tutorial_One_Shot/28_unordered_set.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    unordered_set<int> s;
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+    s.insert(4);
+    s.insert(5);
+    s.insert(1);
+    s.insert(2);
+    s.insert(3);
+
+    cout << "Unordered set elements: ";
+    for (auto val : s) {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+// // Output:
+// Unordered set elements: 3 1 4 2 5
