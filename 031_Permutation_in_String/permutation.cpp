7fea1ac64d1f98567e4e1cc3e8c0bbc725b73e15
Shreya Singh <shreya.official.2.25@gmail.com>
2025-12-10 18:43:22 +0530

    new file:   031_Permutation_in_String/permutation.cpp

---
+++ 031_Permutation_in_String/permutation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isFreqSame(int f1[], int f2[]) {
+        for(int i = 0; i < 26; i++)
+            if(f1[i] != f2[i]) return false;
+        return true;
+    }
+
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
+
+        int freq1[26] = {0}, freq2[26] = {0};
+        int k = s1.length();
+
+        for(char c : s1) freq1[c - 'a']++;
+        for(int i = 0; i < k; i++) freq2[s2[i] - 'a']++;
+
+        if(isFreqSame(freq1, freq2)) return true;
+
+        for(int i = k; i < s2.length(); i++) {
+            freq2[s2[i] - 'a']++;
+            freq2[s2[i - k] - 'a']--;
+            if(isFreqSame(freq1, freq2)) return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    Solution sol;     // object creation
+
+    string s1 = "ab";
+    string s2 = "eidbaooo";
+
+    bool result = sol.checkInclusion(s1, s2);   // function call
+
+    cout << (result ? "true" : "false") << endl;
+    return 0;
+}
